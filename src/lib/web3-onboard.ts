9a1fe8cf3319264fd3b0768264babf276cb41d1a
Adam Carpenter <adamcarpenter86@gmail.com>
2023-03-09 09:07:51 -0700

    Working vite example

--- src/lib/web3-onboard.ts
+++ src/lib/web3-onboard.ts
@@ -4,7 +4,12 @@ import injectedWalletsModule from '@web3-onboard/injected-wallets'
 import walletConnectModule from '@web3-onboard/walletconnect'
 
 const injected = injectedWalletsModule()
-const walletConnect = walletConnectModule()
+const walletConnect = walletConnectModule({    connectFirstChainId: true,
+  version: 2,
+  projectId: 'f6bd6e2911b56f5ac3bc8b2d0e2d7ad5',
+  qrcodeModalOptions: {
+    mobileLinks: ['rainbow', 'metamask', 'argent', 'trust', 'imtoken', 'pillar']
+  }})
 
 const wallets = [injected, walletConnect]
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,60 @@
 import { sveltekit } from '@sveltejs/kit/vite'
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
 import type { UserConfig } from 'vite'
+import Icons from 'unplugin-icons/vite'
+import svg from '@poppanator/sveltekit-svg'
+import nodePolyfills from 'rollup-plugin-polyfill-node'
+import inject from '@rollup/plugin-inject'
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+
+const MODE = process.env.NODE_ENV
+const development = MODE === 'development'
+/** @type {import('@sveltejs/kit').Config} */
 
 const config: UserConfig = {
-  plugins: [sveltekit()],
-  optimizeDeps: {
-    esbuildOptions: {
-      // Node.js global to browser globalThis
-      define: {
-        global: 'globalThis'
-      },
-      // Enable esbuild polyfill plugins
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          buffer: true
-        })
-      ]
-    }
-  }
+	plugins: [
+		sveltekit(),
+		Icons({
+			compiler: 'svelte'
+		}),
+		svg({
+			includePaths: ['./src/lib/assets/']
+		}),
+		development &&
+			nodePolyfills({
+				include: ['node_modules/**/*.js', new RegExp('node_modules/.vite/.*js'), 'http', 'crypto']
+			})
+	],
+	resolve: {
+		alias: {
+			stream: 'stream-browserify',
+			assert: 'assert'
+		}
+	},
+	build: {
+		rollupOptions: {
+			external: ['@web3-onboard/*'],
+			plugins: [nodePolyfills({ include: ['crypto', 'http', 'global'] }), inject({ Buffer: ['Buffer', 'Buffer'] })]
+		},
+		commonjsOptions: {
+			transformMixedEsModules: true
+		}
+	},
+	optimizeDeps: {
+		exclude: ['@ethersproject/hash', 'wrtc', 'http'],
+		include: ['@web3-onboard/core', 'js-sha3', '@ethersproject/bignumber'],
+		esbuildOptions: {
+			// Node.js global to browser globalThis
+			define: {
+				global: 'globalThis'
+			},
+			plugins: [
+				NodeGlobalsPolyfillPlugin({
+					buffer: true
+				})
+			]
+		}
+	},
+
 }
 
 export default config
